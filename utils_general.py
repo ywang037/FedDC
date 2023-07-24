140b9a794d4537de2cb4eed67bcffeea0e2b551d
Frank Wang <32762885+ywang037@users.noreply.github.com>
2023-07-24 18:58:23 +0800

    Update utils_general.py

--- utils_general.py
+++ utils_general.py
@@ -2,10 +2,10 @@ from utils_libs import *
 from utils_dataset import *
 from utils_models import *
 # Global parameters
-os.environ["CUDA_DEVICE_ORDER"]    = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-from torch.utils.tensorboard import SummaryWriter
+# os.environ["CUDA_DEVICE_ORDER"]    = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# from torch.utils.tensorboard import SummaryWriter
 
 import time
 max_norm = 10
